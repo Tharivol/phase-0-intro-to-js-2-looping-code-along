adc0548ec936167c8ea2b1de3e612438c771d187
roberthughes <hughes.brewzz@gmail.com>
2022-02-08 15:52:59 -0700

    commit tree cf23a73bbbc2a4656493746606c158ddc65f2995

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Code your solutions in this file
+// // // Code your solutions in this file
+
+
+//   for (let age =30; age <40; age++) {
+//       console.log('i'm ${age} years onload. Happy birthday to me!');
+      
+//   }
+
+// const gifts = ["teddy bear", "drone", "doll"];
+
+// function wrapGifts(gifts) {
+//   for (let i = 0; i < gifts.length; i++) {
+//     console.log(`Wrapped ${gifts[i]} and added a bow!`);
+//     debugger;
+//   }
+
+//   return gifts;
+// }
+
+// wrapGifts(gifts);
+
+
+const cards = ["card1", "card2"];
+
+function writeCards(cards) {
+    for (let x = 0; x < cards.length; x++) {
+        console.log("here is a card named ${cards[x] } and stuff");
+        debugger;
+    }
+    return gifts;
+}
